38b40503e4e4ebe9f62b068c1ab7cb1c6a1e1547
Daniel Aboderin <aboderindaniel482@gmail.com>
2025-07-02 17:17:57 +0100

    change link

--- components/Header.js
+++ components/Header.js
@@ -166,7 +166,7 @@ export default function Header() {
             className="lg:hidden bg-white border-t border-gray-200 shadow-md"
           >
             <div className="container mx-auto px-6 py-4 space-y-4">
-              {["Home", "Products", "AI LookOut"].map((item, i) => (
+              {["Home", "Products", "LookOut"].map((item, i) => (
                 <Link
                   key={i}
                   href={`/${
